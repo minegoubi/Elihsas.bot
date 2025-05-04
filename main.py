b946185b2bc3dc3a8c4bce4292a974a500172635
minegoubi <amineyagoub648@gmail.com>
2025-05-05 00:43:15 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+from bot import *
+from keep_alive import keep_alive
+
+keep_alive()
+
+# The user provided duplicate code here, I will only include it once.
+# from bot import *
+# from keep_alive import keep_alive
+# keep_alive()
